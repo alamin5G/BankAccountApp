70b491752cc14e5c213afc0cfbcc7b1bda2d2a33
Md Al Amin <alaminrakib4864@gmail.com>
2022-12-31 06:20:35 +0600

    Just do a commit

---
+++ src/BankAccount/Account.java
@@ -0,0 +1,12 @@
+package BankAccount;
+
+public abstract class Account implements IBaseRate{
+
+    //List common properties for savings and checking accounts
+
+    //Constructor to set base properties and initialize the account
+
+    //List Common method.
+
+    //Here is some changes only
+}
